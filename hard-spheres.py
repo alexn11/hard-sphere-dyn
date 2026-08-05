f0c99d39509c2b32cdb77a5081262409a69fcb24
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2026-08-05 21:06:16 +0200

    initial commit

---
+++ hard-spheres.py
@@ -0,0 +1,263 @@
+import subprocess
+
+import numpy as np
+from matplotlib import pyplot
+from matplotlib.animation import FuncAnimation
+
+k_Boltzmann = 1.381e-23
+N_avogadro = 6.022e23
+
+rng = np.random.default_rng()
+
+T = 300
+m = 32 / N_avogadro
+do_tests = False
+
+
+class Maxwellian2D:
+    def __init__(self, m, T, rng=None):
+        self.m = m
+        self.T = T
+        self.rng = rng
+        if(rng is None):
+            self.rng = np.random.default_rng()
+        self.compute_std()
+    def compute_std(self):
+        self.sigma = (self.m / (k_Boltzmann * self.T))**0.5
+    def sample(self, n):
+        return self.rng.normal(0, scale=self.sigma, size=[n, 2])
+
+class CollisionFinder:
+    def __init__(self, x: np.ndarray, v: np.ndarray, t: float, d2: float):
+        self.d2 = d2
+        self.update(x, v, t)
+    def update(self, x: np.ndarray, v: np.ndarray, t: float):
+        self.x = x
+        self.v = v
+        self.t = t
+        self.invalid_collision_time = 2.*t + 1.
+        self.compute_collisions()
+    def compute_collisions(self):
+        # that's quite inefficient, more than half the computations could be
+        # note: there are ways to process symmetric matrices efficiently with numpy
+        # also: not all these values will be needed/used
+        self.dx = self.x[:, np.newaxis, :] - self.x[np.newaxis, :, :]
+        self.dv = self.v[:, np.newaxis, :] - self.v[np.newaxis, :, :]
+        self.dx2 = np.sum(self.dx**2, axis=-1)
+        self.dv2 = np.sum(self.dv**2, axis=-1)
+        self.dxdv = np.sum(self.dx*self.dv, axis=-1)
+        reduced_discriminants = self.dxdv**2 - self.dv2 * (self.dx2 - self.d2)
+        self.filter_discr = reduced_discriminants >= 0.
+        self.collision_times = np.where(self.filter_discr,
+                                   (- self.dxdv - reduced_discriminants**0.5) / self.dv2,
+                                   self.invalid_collision_time)
+        self.filter_t = (self.collision_times >= 0.) & (self.collision_times <= self.t)
+        self.collision_times = np.where(self.filter_t,
+                                        self.collision_times,
+                                        self.invalid_collision_time)
+    def get_next_collision(self) -> None | tuple[tuple[int, int], float]:
+        #print(f'collision_times: {self.collision_times.shape}, argmin={np.argmin(self.collision_times)}')
+        i_1st_collision, j_1st_collision = np.unravel_index(np.argmin(self.collision_times), self.collision_times.shape)
+        t_1st_collision = self.collision_times[i_1st_collision, j_1st_collision]
+        if(t_1st_collision > self.t):
+            return None
+        return (i_1st_collision, j_1st_collision), t_1st_collision
+    def solve_for_collision(self): # note: not to be used
+        i, j = self._i1, self._i2
+        a = self.dv2[i, j]
+        b = self.dxdv[i, j]
+        c = self.dx2[i, j] - self.d2
+        delta = b*b - a*c
+        if(delta < 0):
+            return None
+        t_c = (-b - delta**0.5) / a
+        if((t_c < 0) or (t_c > self.t)):
+            return None
+        return t_c
+
+
+class HardSphereDynamics:
+    def __init__(self, sphere_r, base_dt, z0, box_size=0.):
+        self.r = sphere_r
+        self.d = 2. * self.r
+        self.d2 = self.d**2
+        self.base_dt = base_dt
+        self.x0: np.ndarray = z0[:,0,:]
+        self.v0: np.ndarray = z0[:,1,:]
+        self.box_size = box_size
+        self.prepare_simulation()
+        self.collision_finder = None
+        self.collisions = []
+    def prepare_simulation(self):
+        self.t = 0
+        self.dt = self.base_dt
+        self.x = self.x0.copy()
+        self.v = self.v0.copy()
+        self.N = len(self.x0)
+    def linear_time_step(self):
+        self.x += self.dt * self.v
+        self.t += self.dt
+    def prepare_step(self):
+        self.dt = self.base_dt
+        #self.max_v = self.v.max(0) # i never know so probably axe isnt correct
+        if(self.box_size > 0.):
+            self.x = np.fmod(self.x, self.box_size)
+    def find_next_collision(self):
+        if(self.collision_finder is None):
+            self.collision_finder = CollisionFinder(x=self.x, v=self.v, t=self.dt, d2=self.d2)
+        else:
+            self.collision_finder.update(self.x, self.v, self.dt)
+        next_collision = self.collision_finder.get_next_collision()
+        if(next_collision is None):
+            return None
+        coll_idxs, coll_t = next_collision
+        self.collisions.append([coll_idxs, coll_t + self.t])
+        self.dt = coll_t
+        return coll_idxs
+    def apply_collision(self, collision_idx):
+        i, j = collision_idx
+        collision_dir = self.collision_finder.dx[i, j] / self.d
+        momentum_exchange = np.dot(self.collision_finder.dv[i, j], collision_dir) * collision_dir
+        self.v[i] += - momentum_exchange
+        self.v[j] += momentum_exchange
+    def time_step(self):
+        self.prepare_step()
+        next_collision_i = self.find_next_collision()
+        self.linear_time_step()
+        if(next_collision_i is not None):
+            self.apply_collision(next_collision_i)
+        return self.dt
+
+
+
+def make_collision_graph(collisions: list):
+    particles = list(set(c[0][0] for c in collisions).union(set(c[0][1] for c in collisions)))
+    start_nodes = { i: f'start_{i}' for i in particles }
+    end_nodes = { i: f'end_{i}' for i in particles }
+    collision_nodes = { i: f'c_{i}' for i in range(len(collisions)) }
+    nodes = list(start_nodes.values()) + list(collision_nodes.values()) + list(end_nodes.values())
+    edges = []
+    edge_i = 0
+    particle_lines = {
+        n: []
+        for n in start_nodes
+    }
+    def create_in_edge(c_i, p_i, edge_i):
+        line_of_p_i = particle_lines[p_i]
+        if(line_of_p_i == []):
+            edge_start = start_nodes[p_i]
+            edges.append((edge_start, collision_nodes[c_i]))
+            line_of_p_i.append(edge_i)
+            edge_i += 1
+        else:
+            open_edge = edges[line_of_p_i[-1]]
+            closed_edge = (open_edge[0], collision_nodes[c_i])
+            edges[line_of_p_i[-1]] = closed_edge
+        return edge_i
+    #
+    def create_out_edge(c_i, p_i, edge_i):
+        edge = (collision_nodes[c_i], end_nodes[p_i])
+        edges.append(edge)
+        particle_lines[p_i].append(edge_i)
+        return edge_i+1
+    #
+    for c_i, collision in enumerate(collisions):
+        (p_i, p_j), _ = collision
+        edge_i = create_in_edge(c_i, p_i, edge_i)
+        edge_i = create_in_edge(c_i, p_j, edge_i)
+        edge_i = create_out_edge(c_i, p_i, edge_i)
+        edge_i = create_out_edge(c_i, p_j, edge_i)
+    return nodes, edges
+
+
+
+def generate_dot_file(edges: list[tuple[str, str]], oriented=False):
+    # generate a DOT graph
+    # digraph collisions {
+    #   0 -> 1 [label="123"];
+    #   etc.
+    # }
+    def process_node_name(node_name: str) -> str:
+        #return node_name.replace('_', '<sub>') + '</sub>'
+        return node_name.replace('_', '')
+    if(oriented):
+        dot_content = "digraph"
+    else:
+        dot_content = "graph"
+    dot_content += " collisions {\n  rankdir=BT;\n"
+    edge_symbol = "->" if(oriented) else "--"
+    dot_content += '\n'.join([ f"  {process_node_name(edge[0])} {edge_symbol} {process_node_name(edge[1])};" for edge in edges ])
+    dot_content += '\n}\n'
+    return dot_content
+
+
+# tests
+if(do_tests):
+    # collision detection
+    cf = CollisionFinder(x=np.array([[-1.,0.], [0.,0.]]),
+                        v=np.array([[1.,0.],[0.,0.]]),
+                        t=2.,
+                        d2=0.01)
+    assert(np.allclose(cf.dx[0,0],[0.,0.]))
+    assert(np.allclose(cf.dx[1,0],[1.,0.]))
+    assert(np.allclose(cf.dx[0,1],[-1.,0.]))
+    assert(np.allclose(cf.dx[1,1],[0.,0.]))
+    assert(np.allclose(cf.dv[0,0],[0.,0.]))
+    assert(np.allclose(cf.dv[1,0],[-1.,0.]))
+    assert(np.allclose(cf.dv[0,1],[1.,0.]))
+    assert(np.allclose(cf.dv[1,1],[0.,0.]))
+    assert(np.allclose(cf.dxdv[0,0], 0.))
+    assert(np.allclose(cf.dxdv[1,0], -1.))
+    assert(np.allclose(cf.dxdv[0,1], -1.))
+    assert(np.allclose(cf.dxdv[1,1], 0.))
+    cf.get_next_collision()
+    # collision data to graph
+    collisions = [[(5, 6), 0.0008667870314063772], [(6, 7), 4.531346070838127], [(1, 3), 7.5919666296061115]]
+    nodes, edges = make_collision_graph(collisions)
+    assert(nodes == ['start_1', 'start_3', 'start_5', 'start_6', 'start_7', 'c_0', 'c_1', 'c_2', 'end_1', 'end_3', 'end_5', 'end_6', 'end_7'])
+    assert(edges == [('start_5', 'c_0'), ('start_6', 'c_0'), ('c_0', 'end_5'), ('c_0', 'c_1'), ('start_7', 'c_1'), ('c_1', 'end_6'), ('c_1', 'end_7'), ('start_1', 'c_2'), ('start_3', 'c_2'), ('c_2', 'end_1'), ('c_2', 'end_3')])
+
+
+
+# simulate hard sphere dynamics & display on a plot
+n = 20
+v0 = Maxwellian2D(k_Boltzmann, 1).sample(n)
+x0 = rng.random(size=[n, 2])
+z0 = np.stack([x0, v0], axis=2)
+
+dyn = HardSphereDynamics(sphere_r=1, base_dt=0.1, z0=z0, box_size=2.0)
+
+fig, ax = pyplot.subplots()
+ax.set_xlim(left=0., right=2.0)
+ax.set_ylim(bottom=0., top=2.0)
+scatter_plot = ax.scatter(dyn.x[:,0], dyn.x[:,1])
+
+def plot_update(frame):
+    global scatter_plot
+    dyn.time_step()
+    scatter_plot.set_offsets(list(zip(dyn.x[:,0], dyn.x[:,1])))
+
+
+animation = FuncAnimation(fig, plot_update, frames = None)
+pyplot.show()
+
+# when stopped show the number of collisions with time
+
+collision_times = np.array([c[-1] for c in dyn.collisions])
+collision_counts = np.arange(len(collision_times)) + 1
+pyplot.plot(collision_times, collision_counts)
+pyplot.show()
+
+
+# create a graph of the collisions
+
+nodes, edges = make_collision_graph(dyn.collisions)
+
+dot_content = generate_dot_file(edges)
+print(dot_content)
+
+with open('sample.dot', 'w') as dot_file:
+    dot_file.write(dot_content)
+
+# dot -Tsvg  sample.dot  > sample.svg
+subprocess.run(['dot', '-Tsvg', 'sample.dot', '-o', 'sample.svg'])
