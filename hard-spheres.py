bfb26c295bd660bc3531195a09255173d0bf696e
Alexandre DeZotti <dezotti.alexandre@gmail.com>
2026-09-18 20:50:25 +0200

    attemps

--- hard-spheres.py
+++ hard-spheres.py
@@ -179,6 +179,14 @@ def generate_dot_file(edges: list[tuple[str, str]], oriented=False):
     dot_content += '\n}\n'
     return dot_content
 
+def compute_marker_size(fig: pyplot.Figure, ax: pyplot.Axes, marker_real_size: float):
+    b = ax.transData._b.get_matrix()
+    print(f'b={b[:2,:2]}')
+    pixels_per_unit_x = b[0,0]
+    pixels_per_unit_y = b[1,1]
+    marker_size = 13.5 * (fig.dpi**2) * (marker_real_size**2) / (pixels_per_unit_x * pixels_per_unit_y)
+    print(f's={marker_size}')
+    return marker_size
 
 
 if __name__ == '__main__':
@@ -243,55 +251,34 @@ if __name__ == '__main__':
 
     dyn = HardSphereDynamics(sphere_r=1, base_dt=0.1, z0=z0, box_size=box_size)
 
-    fig, ax = pyplot.subplots()
+    fig, ax = pyplot.subplots(dpi=300)
+    ticks = np.arange(start=0, stop=box_size, step=1.)
     ax.set_xlim(left=0., right=box_size)
     ax.set_ylim(bottom=0., top=box_size)
-    circles = []
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    scatter_plot = ax.scatter(dyn.x[:,0], dyn.x[:,1],
+                              edgecolor='k',
+                              linewidths=1.)
+    marker_size = compute_marker_size(fig, ax, sphere_r)
+    ax.clear()
+    ax.set_xlim(left=0., right=box_size)
+    ax.set_ylim(bottom=0., top=box_size)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    scatter_plot = ax.scatter(dyn.x[:,0], dyn.x[:,1],
+                              edgecolor='k',
+                              linewidths=1.,
+                              s=marker_size)
 
-    def draw_circle_scatter(ax: pyplot.Axes, circles: list[matplotlib.patches.Circle], centers: np.ndarray):
-        if(circles != []):
-            for circle in circles:
-                circle.remove()
-            circles.clear()
-        circles += [
-            pyplot.Circle(centers[i], radius=sphere_r, facecolor='red', edgecolor='k', lw=1.)
-            for i in range(len(centers))
-        ]
-        for circle in circles:
-            ax.add_patch(circle)
-        return 
-        if(len(circles) == 0):
-            circle_objects = [
-                pyplot.Circle(centers[i], radius=sphere_r, facecolor='red', edgecolor='k', lw=1.)
-                for i in range(len(centers))
-            ]
-            circles = [ ax.add_patch(circle) for circle in circle_objects ]
-        else:
-            for i, circle in enumerate(circles):
-                circle.set_center(centers[i])
-            #for patch in ax.patches:
-            #    ax.patches.
-            for circle in circles:
-                ax.add_patch(circle)
-
-
-    def plot_update(frame, ax: pyplot.Axes = None, circles: list[pyplot.Circle] = None, x: np.ndarray = None):
-        global fig
+    def update_scatter(frame,
+                       scatter_plot: matplotlib.collections.PathCollection = None,
+                       dyn: HardSphereDynamics = None):
         dyn.time_step()
-        draw_circle_scatter(ax, circles, x)
-        #ax.draw(fig.canvas.renderer)
-        fig.canvas.flush_events()
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        #draw_circle_scatter(ax, [], x)
-
-    def plot_update_scatter(frame, x: np.ndarray = None):
-        global scatter_plot
-        dyn.time_step()
-        scatter_plot.set_offsets(list(zip(x[:,0], x[:,1])))
+        scatter_plot.set_offsets(list(zip(dyn.x[:,0], dyn.x[:,1])))
 
     animation = FuncAnimation(fig=fig,
-                              func=partial(plot_update, ax=ax, circles=circles, x=dyn.x),
+                              func=partial(update_scatter, scatter_plot=scatter_plot, dyn=dyn),
                               frames=None)
     pyplot.show()
 
